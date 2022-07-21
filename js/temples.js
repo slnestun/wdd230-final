1ccebbec0735e9615a508522feffe6d15aa409ce
slnestun <riv21007@byui.edu>
2022-07-20 21:46:17 -0300

    more fixes

--- js/temples.js
+++ js/temples.js
@@ -15,8 +15,11 @@ function displayTemples(temple){
     cardView.setAttribute("class", "temple-card");
     let imgDiv = document.createElement("div");
     imgDiv.setAttribute("class","img-temple");
+    let picture = document.createElement("picture");
+
     let image = document.createElement("img");
-    image.setAttribute("src", temple.imageUrl);
+    image.setAttribute("data-src",temple.imageUrl)
+    image.setAttribute("src", "https://via.placeholder.com/600X400.webp/000000/FFFFFF/?text=600X400");
     image.setAttribute("alt", `${temple.templeName} Logo`);
     image.setAttribute("height", `400`);
     image.setAttribute("width", `600`);
@@ -51,7 +54,8 @@ function displayTemples(temple){
     likeView.textContent = `${likeCounter} Likes`
     
     cardView.appendChild(imgDiv);
-    imgDiv.appendChild(image);
+    imgDiv.appendChild(picture);
+    picture.appendChild(image);
     cardView.appendChild(hover);
     cardView.appendChild(container);
     container.appendChild(h3);
